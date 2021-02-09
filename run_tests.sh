877eec3859b5633d4b05694d881ef90e0db4c5e1
phartenfeller <pharti@hotmail.de>
2021-02-09 12:15:44 +0100

    changed directory rights in dockerfile

--- run_tests.sh
+++ run_tests.sh
@@ -15,4 +15,9 @@ docker run --rm --security-opt seccomp=$(pwd)/chrome-seccomp.json -v $(pwd)/test
 
 file_exists ./test/sample/output/Chromium_Whatsmybrowser.png
 
+if ls ./test/sample/output/*.failed.* 1>/dev/null 2>&1; then
+  echo "One ore more tests failed"
+  exit 1
+fi
+
 echo "Tests completed successfully..."
